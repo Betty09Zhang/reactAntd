d2e8cb93be576c1c9065611dd853fa2c87e236e0
BettyZhang <2695765705@qq.com>
2023-10-18 23:26:52 +0800

    feat：补充菜单组件

--- src/component/Menu/index.tsx
+++ src/component/Menu/index.tsx
@@ -1,33 +1,34 @@
 
-// type MenuMode = 'horizontal' | 'vertical'
-// interface MenuProps {
-//     activeIndex?: number;
-//     className?: string;
-//     onSelect?: (index: number) => void;
-//     mode?: MenuMode;
-//     style?: React.CSSProperties;
-// }
-// const Menu: React.FC<MenuProps> = (props) => {
-//     const { activeIndex, onSelect, children, defaultIndex } = props
-//     return (
-//        <ul>
-//            { children }
-//        </ul>             
-//     )
+import React from "react";
+type MenuMode = 'horizontal' | 'vertical'
+interface MenuProps {
+    activeIndex?: number;
+    className?: string;
+    onSelect?: (index: number) => void;
+    mode?: MenuMode;
+    style?: React.CSSProperties;
+    children: React.ReactNode
+}
+export const Menu: React.FC<MenuProps> = (props) => {
+    const { activeIndex, onSelect, children, defaultIndex } = props
+    return (
+       <ul>
+           { children }
+       </ul>             
+    )
        
-// }
-// interface MenuItemProps {
-//     index: number,
-//     disabled: boolean,
-// }
-// const MenuItem = (props) => {
-//     const children = props.children 
-//     return(
-//         <li>
-//             {children}
-//         </li>
-//     )
-// }
+}
+interface MenuItemProps {
+    index: number,
+    disabled: boolean,
+    children: React.ReactNode
+}
+const MenuItem = (props: MenuItemProps) => {
+    const children = props.children 
+    return(
+        <li>
+            {children}
+        </li>
+    )
+}
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {}
