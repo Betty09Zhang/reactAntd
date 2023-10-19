ae34d921e195ce1e6cd6331eef0837ed5649a0ae
BettyZhang <2695765705@qq.com>
2023-10-19 23:15:32 +0800

    feat: menu菜单逻辑

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { BtnSize, Button, ButtonType } from './component/Button';
 // import './App.css';
+import { SubMenu, Menu, MenuItem } from './component/Menu';
 function App() {
   return (
     <div className="App">
+      <Menu>
+        <MenuItem index={0} disabled={false} >111</MenuItem>
+        <SubMenu></SubMenu>
+      </Menu>
       <Button onClick={() => alert(11)}>Default</Button>
       <Button disabled>Default disabled</Button>
       <Button btnType={ ButtonType.Success} size={ BtnSize.Large}>Test</Button>
--- src/component/Menu/index.tsx
+++ src/component/Menu/index.tsx
@@ -7,10 +7,10 @@ interface MenuProps {
     onSelect?: (index: number) => void;
     mode?: MenuMode;
     style?: React.CSSProperties;
-    children: React.ReactNode
+    children?: React.ReactNode
 }
 export const Menu: React.FC<MenuProps> = (props) => {
-    const { activeIndex, onSelect, children, defaultIndex } = props
+    const { activeIndex, onSelect, children } = props
     return (
        <ul>
            { children }
@@ -20,10 +20,10 @@ export const Menu: React.FC<MenuProps> = (props) => {
 }
 interface MenuItemProps {
     index: number,
-    disabled: boolean,
-    children: React.ReactNode
+    disabled?: boolean,
+    children?: React.ReactNode
 }
-const MenuItem = (props: MenuItemProps) => {
+export const MenuItem = (props: MenuItemProps) => {
     const children = props.children 
     return(
         <li>
@@ -31,4 +31,18 @@ const MenuItem = (props: MenuItemProps) => {
         </li>
     )
 }
+export const SubMenu = (props:MenuProps) => {
+    const data = [{ title: '123'}, { title: '456'}, { title: '789'}]
+    const WrapperCom = data.map((item, index) => {
+        return (
+            <MenuItem index={index}   >{item.title}</MenuItem>
+        )
+    })
+    return (
+        <Menu>
+            {WrapperCom}
+        </Menu>
+    )
+}
+
 
